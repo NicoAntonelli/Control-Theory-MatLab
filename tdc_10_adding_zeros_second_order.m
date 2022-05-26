1a7964c6880e2418e235d206b20b70cd7201227a
Nicolás Antonelli <niconelli2@gmail.com>
2022-05-26 04:02:26 -0300

    Improved EJ10

--- tdc_10_adding_zeros_second_order.m
+++ tdc_10_adding_zeros_second_order.m
@@ -24,29 +24,33 @@ z = -6;
 [num,den] = zp2tf(z, p, 1/6);
 G3 = tf(num, den);
 
+z= -3;
+[num,den] = zp2tf(z, p, 1/3);
+G4 = tf(num, den);
+
 z = -1;
 [num,den] = zp2tf(z, p, 1);
-G4 = tf(num, den);
+G5 = tf(num, den);
  
 z = -1/2;
 [num,den] = zp2tf(z, p,2);
-G5 = tf(num, den);
+G6 = tf(num, den);
 
 z = 2;
 [num,den] = zp2tf(z, p, -1/2);
-G6 = tf(num, den);
+G7 = tf(num, den);
 
 % Step Responses
-step(G1,'-r', G2, '-g', G3, '-b', G4, '-c', G5, 'm', G6, 'y');
+step(G1,'-r', G2, '-g', G3, '-b', G4, '-c', G5, 'm', G6, 'y', G7, 'k');
 
 xlabel('Time');
 ylabel('Amplitude');
 title('Effects of a Zero adition - Second Order System');
-legend('No Zeros', 'z = -50', 'z = -6', 'z = -1', 'z = -1/2', 'z = 3');
+legend('No Zeros', 'z = -50', 'z = -6', 'z = -3', 'z = -1', 'z = -1/2', 'z = 2');
 
 hold off;
 
 % Pole-Zero map
 figure(2);
-pzmap(G1, G2, G3, G4, G5, G6);
-legend('No Zeros', 'z = -50', 'z = -6', 'z = -1', 'z = -1/2', 'z = 3');
+pzmap(G1, G2, G3, G4, G5, G6, G7);
+legend('No Zeros', 'z = -50', 'z = -6', 'z = -3', 'z = -1', 'z = -1/2', 'z = 2');
